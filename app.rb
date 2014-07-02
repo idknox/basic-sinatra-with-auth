0cbd3126eaa6a158948612ba2d5686a164262ae4
Ian Knox <knoxid@gmail.com>
2014-07-02 11:54:01 -0600

    added private methods to app

--- app.rb
+++ app.rb
@@ -2,9 +2,9 @@ require "sinatra"
 require "rack-flash"
 
 require "./lib/user_database"
-require "./lib/methods.rb"
 
 class App < Sinatra::Application
+
   enable :sessions
   use Rack::Flash
 
@@ -29,6 +29,34 @@ class App < Sinatra::Application
 
   post "/" do
     check_input(params[:username], params[:password])
+  end
+
+  private
+
+  def check_input(username, password)
+    if username == '' && password == ''
+      flash[:notice] = "Username and password is required"
+      redirect "/"
+    elsif username == ''
+      flash[:notice] = "Username is required"
+      redirect "/"
+    elsif password == ''
+      flash[:notice] = "Password is required"
+      redirect "/"
+    else
+      erb :loggedin, :locals => {:username => username}
+    end
+  end
+
+  def check_user(username, password)
+    if (@user_database.all).select { |user| user[:username] == username } == []
+      @user_database.insert(:username => username, :password => password)
+      flash[:notice] = "Thank you for registering"
+      redirect "/"
+    else
+      flash[:notice] = "That username is already taken"
+      redirect "/register"
+    end
   end
 
 end
