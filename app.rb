f5d279a2bd48fbf5d3a2d4d1e81849dc0bcfe2f4
Ian Knox <knoxid@gmail.com>
2014-07-03 12:11:12 -0600

    login checks for registered user /w password

--- app.rb
+++ app.rb
@@ -13,6 +13,9 @@ class App < Sinatra::Application
     @user_database = UserDatabase.new
   end
 
+  before do
+
+  end
   get "/" do
 
   erb :homepage
@@ -31,6 +34,14 @@ class App < Sinatra::Application
     check_input(params[:username], params[:password])
   end
 
+  # delete "/" do
+  # name = params[:name]
+  #
+  # flash[:notice] = "#{name}"
+
+
+
+
   private
 
   def check_input(username, password)
@@ -43,19 +54,43 @@ class App < Sinatra::Application
     elsif password == ''
       flash[:notice] = "Password is required"
       redirect "/"
+    elsif !get_id(username)
+      flash[:notice] = "Not a registered user"
+      redirect "/"
+    elsif get_id(username) and !check_pw(password)
+      flash[:notice] = "Invalid password"
+      redirect "/"
     else
-      erb :loggedin, :locals => {:username => username}
+      erb :loggedin, :locals => {:users => display_users(username), :cur_user => username}
     end
   end
 
   def check_user(username, password)
     if (@user_database.all).select { |user| user[:username] == username } == []
-      @user_database.insert(:username => username, :password => password)
+      user = @user_database.insert(:username => username, :password => password)
       flash[:notice] = "Thank you for registering"
       redirect "/"
     else
       flash[:notice] = "That username is already taken"
       redirect "/register"
+    end
+  end
+
+  def display_users(cur_user)
+    @user_database.all.select {|user| user[:username] != cur_user}
+  end
+
+  def get_id(username)
+    valid_user = @user_database.all.select {|user| user[:username] == username}
+    if valid_user != []
+      valid_user[0][:id]
+    end
+  end
+
+  def check_pw(password)
+    pw = @user_database.all.select {|user| user[:password] == password}
+    if pw != []
+      pw
     end
   end
 
